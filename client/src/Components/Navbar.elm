1b716bdb4a9e93f0f3cf020d2b90f8af59db91fb
Vedanth Ramji <86137377+Vedanth-Ramji@users.noreply.github.com>
2024-11-19 12:33:17 +0530

    ADD genomes & taxonomy pages
    * ADD genomes & taxonomy pages

--- client/src/Components/Navbar.elm
+++ client/src/Components/Navbar.elm
@@ -5,10 +5,10 @@ import Html.Attributes as Hattr exposing (..)
 navbar : Html msg
 navbar =
     div [Hattr.class "navbar"] [
-        h2 [Hattr.class "navbar_title"] [text "MagsView"],
+        h2 [Hattr.class "navbar_title"] [a [Hattr.href "/"] [text "MagsView"]],
         div [Hattr.class "navbar_links"] [
             a [Hattr.href "/"] [text "Home"],
-            a [Hattr.href "#"] [text "Genomes"],
-            a [Hattr.href "#"] [text "Taxonomy"]
+            a [Hattr.href "/genomes"] [text "Genomes"],
+            a [Hattr.href "/taxonomy"] [text "Taxonomy"]
         ]
     ]
